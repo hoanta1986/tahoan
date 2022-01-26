51766a81e54bace15deeebcebc36c0e9feb09492
hoan <hoanta12393@gmail.com>
2022-01-26 11:31:34 +0700

    Components Part 1

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,16 +1,22 @@
+// eslint-disable-next-line
 import logo from './logo.svg';
-import{ Navbar, NavbarBrand } from "reactstrap";
+
+import Menu from './components/MenuComponent';
+import { Navbar, NavbarBrand } from "reactstrap";
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-  <Navbar dark color="primary">
-    <div className="container">
-      <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+    <div>
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
+      <Menu />
     </div>
-  </Navbar>
-    </div>
+
   );
 }
 
