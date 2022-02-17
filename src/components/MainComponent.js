e34c39a3c8e7706ff49eb62bee2d880358b28a25
hoan <hoanta12393@gmail.com>
2022-02-17 17:35:12 +0700

    Single Page Applications Part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,7 +3,7 @@
 import React, { Component } from 'react';
 
 import Menu from './MenuComponent';
-// eslint-disable-next-line
+
 import DishDetail from './DishdetailComponent';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
@@ -34,13 +34,22 @@ class Main extends Component {
           />
       );
     }
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
+
     return (
       <div className="App">
      <Header />
      <Switch>
               <Route path="/home" component={HomePage} />
               <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+              <Route path="/menu/:dishId" component={DishWithId} />
               <Route exact path="/contactus" component={Contact}  />
+    
               <Redirect to="/home" />
           </Switch>
         <Footer />
